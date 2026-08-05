18e543dbc89216cc6098ff3dddb58f538bf3d34d
Nilvan Peres Jr <54211866+NilvanPeres@users.noreply.github.com>
2026-08-05 10:16:33 -0300

    Add: number-of-students-unable-to-eat-lunch - submission-8

---
+++ Data Structures & Algorithms/number-of-students-unable-to-eat-lunch/submission-8.py
@@ -0,0 +1,20 @@
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        result = len(students)
+        cnt = {}
+
+        # Dict para saber de match entre aluno e tipo de sanduiche
+        for s in students:
+            if s not in cnt:
+                cnt[s] = 0
+            cnt[s] +=1
+
+        # Itera sobre as comidas e vê se há aluno compatível na fila...
+        for s in sandwiches:
+            if cnt.get(s, 0) > 0:
+                result -= 1 # Tira aluno da fila
+                cnt[s] -= 1 #Atualiza 'value' da respectiva do key do dict
+            else:
+                return result #Early exit
+        
+        return result
